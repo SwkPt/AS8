13e46aafa6804f33c0c52ac97deac53accec7359
SwkPt <swk.pt16@gmail.com>
2026-03-18 13:44:21 +0700

    path

--- src/libs/getVenues.tsx
+++ src/libs/getVenues.tsx
@@ -2,7 +2,7 @@ export default async function getVenues() {
     
     await new Promise( (resolve)=>setTimeout(resolve,2000) ) 
 
-    const response = await fetch("https://a08-venue-explorer-backend.vercel.app/api/v1/venues")
+    const response = await fetch("https://a08-venue-explorer-backend-2.vercel.app/api/v1/venues")
     if(!response.ok) throw new Error("fetch venue failed")
 
     return await response.json()
